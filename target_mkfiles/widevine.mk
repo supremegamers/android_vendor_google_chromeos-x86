1baf02ea9b089c787148dbcaa1d3300e5811a583
AXON <axonasif@gmail.com>
2022-04-14 21:47:23 +0000

    Update

---
+++ target_mkfiles/widevine.mk
@@ -0,0 +1,7 @@
+# Bundle Widevine DRM
+WIDEVINE_PATH := $(dir $(LOCAL_PATH))proprietary/widevine/vendor
+
+PRODUCT_COPY_FILES += \
+    $(WIDEVINE_PATH)/bin/hw/android.hardware.drm@1.1-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.1-service.widevine:widevine \
+    $(WIDEVINE_PATH)/etc/init/android.hardware.drm@1.1-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.1-service.widevine.rc:widevine \
+    $(WIDEVINE_PATH)/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so:widevine
